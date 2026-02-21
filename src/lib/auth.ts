12bf92ce1896ad8315cd2246bd3a5b2a1ef53987
Your Name <you@example.com>
2026-02-21 18:54:51 +0530

    fix(auth): enable allowDangerousEmailAccountLinking to allow existing credential users to merge with google logins seamlessly

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -24,6 +24,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            allowDangerousEmailAccountLinking: true,
         }),
         CredentialsProvider({
             name: 'Credentials',
